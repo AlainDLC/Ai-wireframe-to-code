a32ec9244ee634b612a8e057b424b8146335fe96
AlainDLC <jd_alain@hotmail.com>
2025-02-08 21:38:39 +0100

    fix some issue with starter file

--- ai-code/app/provider.tsx
+++ ai-code/app/provider.tsx
@@ -1,20 +1,21 @@
-"use client"
-import { auth } from '@/configs/firebaseConfig';
-import { AuthContext } from '@/context/AuthContext';
-import { onAuthStateChanged, User } from 'firebase/auth';
-import React, { useContext, useEffect, useState } from 'react'
+"use client";
+import { auth } from "@/configs/firebaseConfig";
+import { AuthContext } from "@/context/AuthContext";
+import { onAuthStateChanged, User } from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
-    user: User | null;
+  user: User | null;
 }
 
 function Provider({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
+  /*
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             setUser(user);
@@ -22,22 +23,19 @@ function Provider({
 
         return () => unsubscribe(); // Cleanup
     }, []);
-
-    return (
-        <AuthContext.Provider value={{ user }}>
-            <div>
-                {children}
-            </div>
-        </AuthContext.Provider>
-    )
+*/
+  return (
+    <AuthContext.Provider value={{ user }}>
+      <div>{children}</div>
+    </AuthContext.Provider>
+  );
 }
 
 // Custom hook to use auth
 export const useAuthContext = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (!context) throw new Error("useAuth must be used within an AuthProvider");
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
 };
 
-export default Provider
-
+export default Provider;
